85d7d4a6b7012f1419b2911dc17626750452dce3
Hampus Nilsson <hn222te@student.lnu.se>
2023-04-03 13:47:34 +0200

    Adjust header

--- lekstugan-client/src/app/app.component.ts
+++ lekstugan-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isSticky: boolean = false;
   title = 'lekstugan-client';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    this.isSticky = scrollTop > 50;
+  }
 }
