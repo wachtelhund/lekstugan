995c193742f13effa7e08ac5126292c98afece04
Hampus Nilsson <hn222te@student.lnu.se>
2023-04-18 13:21:37 +0200

    Set up basic get route for images

--- lekstugan-server/src/controllers/v1/ImageController.ts
+++ lekstugan-server/src/controllers/v1/ImageController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import Image from '../../models/mongo/Image';
 /**
  * ImageController
  */
@@ -10,7 +11,28 @@ export class ImageController {
    * @param {Request} res - Response
    * @param {NextFunction} _next - NextFunction
    */
-  public getAll(_req: Request, res: Response, _next: NextFunction): void {
-    res.send('ImageController.index');
+  public async getAll(
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void> {
+    const images = await Image.find({});
+    res.json(images);
+  }
+
+  /**
+   * Post a new image
+   *
+   * @param {Request} req - Request
+   * @param {Request} res - Response
+   * @param {NextFunction} _next - NextFunction
+   */
+  public async post(
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void> {
+    const image = await Image.create(req.body);
+    res.json(image);
   }
 }
--- lekstugan-server/src/index.ts
+++ lekstugan-server/src/index.ts
@@ -3,45 +3,53 @@ import helmet from 'helmet';
 import logger from 'morgan';
 import { config } from 'dotenv';
 import { router } from './routes/api/router';
+import mongoose from 'mongoose';
+config();
 
-try {
-  const app = express();
-  config();
-  app.use(helmet());
-  app.use(logger('dev'));
+if (process.env.DB_CONNECTION_STRING !== undefined) {
+  mongoose
+    .connect(process.env.DB_CONNECTION_STRING)
+    .then(() => {
+      const app = express();
+      app.use(helmet());
+      app.use(logger('dev'));
 
-  const PORT = process.env.PORT || 3000;
+      const PORT = process.env.PORT || 3000;
 
-  app.use('/', router);
+      app.use('/', router);
 
-  const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
-    err.status = err.status || 500;
-    if (req.app.get('env') !== 'development') {
-      return res.status(err.status).json({
-        status: err.status,
-        message: err.message,
+      const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
+        err.status = err.status || 500;
+        if (req.app.get('env') !== 'development') {
+          return res.status(err.status).json({
+            status: err.status,
+            message: err.message,
+          });
+        }
+
+        return res.status(err.status).json({
+          status: err.status,
+          message: err.message,
+          cause: err.cause
+            ? {
+                status: err.cause.status,
+                message: err.cause.message,
+                stack: err.cause.stack,
+              }
+            : null,
+          stack: err.stack,
+        });
+      };
+
+      app.use(errorHandler);
+
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
       });
-    }
-
-    return res.status(err.status).json({
-      status: err.status,
-      message: err.message,
-      cause: err.cause
-        ? {
-            status: err.cause.status,
-            message: err.cause.message,
-            stack: err.cause.stack,
-          }
-        : null,
-      stack: err.stack,
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  };
-
-  app.use(errorHandler);
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-} catch (error) {
-  console.log(error);
+} else {
+  throw new Error('Missing db connection string, exiting');
 }
