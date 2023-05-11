3fce454c9bbea852928945b053d2d6663e437e4d
Hampus Nilsson <hn222te@student.lnu.se>
2023-05-11 15:31:47 +0200

    Added admin registration

---
+++ lekstugan-client/src/app/components/admin-authenticator/admin-authenticator.component.ts
@@ -0,0 +1,60 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from 'src/app/services/auth.service';
+import {IUser} from 'src/app/types/IUser';
+
+@Component({
+  selector: 'app-admin-authenticator',
+  templateUrl: './admin-authenticator.component.html',
+  styleUrls: ['./admin-authenticator.component.scss'],
+})
+/**
+ * The admin authenticator component. Used to create
+ * and delete admins.
+ */
+export class AdminAuthenticatorComponent {
+  admins: string[] = [];
+  addAdminForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  /**
+   * Constructor.
+   */
+  constructor(private auth: AuthService) {}
+
+  /**
+   * On init.
+   */
+  ngOnInit() {
+    this.auth.getAdmins().subscribe((admins) => {
+      this.admins.push(...admins);
+    });
+  }
+
+  /**
+   * On delete.
+   *
+   * @param {string} adminMail The event.
+   */
+  onDelete(adminMail: string) {
+    console.log(adminMail);
+    this.auth.deleteAdmin(adminMail).subscribe(() => {
+      this.admins = this.admins.filter((admin) => admin !== adminMail);
+    });
+  }
+
+  /**
+   * On add.
+   */
+  onAdd() {
+    if (this.addAdminForm.invalid) {
+      throw new Error('Invalid form');
+    }
+    this.auth.addAdmin(this.addAdminForm.value as IUser).subscribe((res) => {
+      console.log(res);
+      this.admins.push((this.addAdminForm.value as IUser).email);
+    });
+  }
+}
--- lekstugan-client/src/app/services/auth.service.ts
+++ lekstugan-client/src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import {IUser} from '../types/IUser';
 import {IUserPayload} from '../types/IUserPayload';
 import {environment} from '../../environments/environment';
 import jwtDecode, {JwtPayload} from 'jwt-decode';
-import {BehaviorSubject, catchError, map, of} from 'rxjs';
+import {BehaviorSubject, Observable, catchError, map, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -47,8 +47,43 @@ export class AuthService {
             })
         );
   }
-}
 
+  /**
+   * Gets all registered admins.
+   *
+   * @return {Observable<any>} - The response.
+   */
+  getAdmins(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiURL}/users`,
+    // eslint-disable-next-line
+      {headers: {Authorization: 'Bearer ' + this.token.value}});
+  }
+
+  /**
+   * Remove an admin.
+   *
+   * @param {string} email - The email of the admin to remove.
+   * @return {Observable<any>} - The response.
+   */
+  deleteAdmin(email: string): Observable<any> {
+    return this.http.delete(`${this.apiURL}/${email}`,
+    // eslint-disable-next-line
+    {headers: {Authorization: 'Bearer ' + this.token.value}});
+  }
+
+  /**
+   * Add an admin.
+   *
+   * @param {IUser} user - The user to add.
+   * @return {Observable<any>} - The response.
+   */
+  addAdmin(user: IUser): Observable<any> {
+    return this.http.post(`${this.apiURL}/add`,
+        {user},
+        // eslint-disable-next-line
+        {headers: {Authorization: 'Bearer ' + this.token.value}});
+  }
+}
 export interface ILoginResponse {
   // eslint-disable-next-line
   access_token: string;
