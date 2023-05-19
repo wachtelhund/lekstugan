c07dc4914c55609cc969ce165f50f682182edaca
Hampus Nilsson <hn222te@student.lnu.se>
2023-05-19 11:59:57 +0200

    Feature/automail

---
+++ lekstugan-server/src/models/types/IMailParams.ts
@@ -0,0 +1,10 @@
+export interface IMailParams {
+  to_email: string;
+  to_name: string;
+  from_name: string;
+  message?: string;
+  status?: string;
+  date?: string;
+  key?: string;
+  reply_to?: string;
+}
